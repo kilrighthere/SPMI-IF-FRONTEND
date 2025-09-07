eec4b946682e5e8d21306fe15471945c087eab09
Shakila Tungga Dewi <shakilatunggadewi731@gmail.com>
2025-09-07 20:30:46 +0700

    Update route sidebar and wrapper data sub menu

--- src/views/Kurikulum/UkurCPL.vue
+++ src/views/Kurikulum/UkurCPL.vue
@@ -1,12 +1,12 @@
 <script setup>
 // import { useRouter } from 'vue-router'
-import Footer from '@/components/Footer.vue';
+import Footer from '@/components/Footer.vue'
 import Header from '@/components/Header.vue'
-import Sidebar from '@/components/Sidebar.vue';
+import Sidebar from '@/components/Sidebar.vue'
 </script>
 
 <template>
-    <!-- <div class="dash-container">
+  <!-- <div class="dash-container">
         <Sidebar/>
         <div class="main-content">
             <Header/>
@@ -20,23 +20,23 @@ import Sidebar from '@/components/Sidebar.vue';
 
 <style>
 .dash-container {
-    display: flex;
-    min-height: 100vh;
+  display: flex;
+  min-height: 100vh;
 }
 
 .main-content {
-    flex: 1;
-    margin-left: 256px; /* Same as sidebar width */
-    margin-top: 0; /* No need for margin-top as we have padding-top in .dashboard */
-    display: flex;
-    flex-direction: column;
+  flex: 1;
+  margin-left: 256px; /* Same as sidebar width */
+  margin-top: 0; /* No need for margin-top as we have padding-top in .dashboard */
+  display: flex;
+  flex-direction: column;
 }
 
 .dashboard {
-    flex: 1; /* Take up available space */
-    padding-top: 90px; /* Adjusted for new header height */
-    padding-left: 30px;
-    padding-right: 30px;
-    padding-bottom: 50px; /* Add bottom padding for footer space */
+  flex: 1; /* Take up available space */
+  padding-top: 90px; /* Adjusted for new header height */
+  padding-left: 30px;
+  padding-right: 30px;
+  padding-bottom: 50px; /* Add bottom padding for footer space */
 }
 </style>
